437f3576df37f66406d37a2579d28a2f80e5f705
Mainul islam <mainulisraf163@gmail.com>
2022-05-13 23:36:31 +0600

    second life

--- src/component/ReviewPage.js
+++ src/component/ReviewPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ReviewPage = () => {
-  return <div>hlw I am review page just push</div>;
+  return <div>hlw I am review page.... second life </div>;
 };
 
 export default ReviewPage;
